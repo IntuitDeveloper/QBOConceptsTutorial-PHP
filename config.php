da22bbd51fb1f6320845bf1c2a5dd9542a3c62b4
Hao Lu <Hao_Lu@intuit.com>
2018-04-17 10:05:41 -0700

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return array(
     'tokenEndPointUrl' => 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
     'client_id' => 'Q0fXL014zAv3wzmlhwXMEHTrKepfAshCRjztEu58ZokzCD5T7D',
     'client_secret' => 'stfnZfuSZUDay6cJSWtvQ9HkWiKFbcI9YuBTET5P',
-    'oauth_scope' => 'com.intuit.quickbooks.accounting com.intuit.quickbooks.payment openid profile email phone address',
+    'oauth_scope' => 'com.intuit.quickbooks.accounting openid profile email phone address',
     'oauth_redirect_uri' => 'http://localhost:3000/callback.php',
 )
 ?>
