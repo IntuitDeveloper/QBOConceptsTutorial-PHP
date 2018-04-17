55d92102a4802940fb7e9ab9fa5f47ea3ac64818
milin6288 <milin6288@gmail.com>
2018-04-17 12:19:32 -0700

    Update Accounting.php

--- Accounting.php
+++ Accounting.php
@@ -5,6 +5,9 @@ use QuickBooksOnline\API\DataService\DataService;
 use QuickBooksOnline\API\Core\Http\Serialization\XmlObjectSerializer;
 use QuickBooksOnline\API\Facades\Account;
 use QuickBooksOnline\API\Facades\JournalEntry;
+// Import Facade classes you are going to use here
+// For example, if you need to use Customer, add
+// use QuickBooksOnline\API\Facades\Customer;
 
 session_start();
 
@@ -21,21 +24,21 @@ function accounting()
         'scope' => $config['oauth_scope'],
         'baseUrl' => "development"
     ));
-    /*
-     * Retrieve the accessToken value from session variable
-     */
+
+     // Retrieve the accessToken value from session variable
+
     $accessToken = $_SESSION['sessionAccessToken'];
     $dataService->throwExceptionOnError(true);
-    /*
-     * Update the OAuth2Token of the dataService object
-     */
+
+     // Update the OAuth2Token of the dataService object
+
     $dataService->updateOAuth2Token($accessToken);
 
-    //Start write your business logic here, and store the final result to $result object
-    //Create  bank account
+    // Start write your business logic here, and store the final result to $result object
+    // Create  bank account
     $theResourceObj = Account::create([
       "AccountType" => "Bank",
-      "Name" => "Milin Bank account7"
+      "Name" => "Bank account7"
     ]);
     $resultingObj = $dataService->Add($theResourceObj);
     $error = $dataService->getLastError();
@@ -49,10 +52,10 @@ function accounting()
         $xmlBody = XmlObjectSerializer::getPostXmlFromArbitraryEntity($resultingObj, $urlResource);
         print $xmlBody . "\n";
     }
-    //Create credit card account
+    // Create credit card account
     $theResourceObj = Account::create([
         "AccountType" => "Credit Card",
-        "Name" => "Milin Credit card account7"
+        "Name" => "Credit card account7"
     ]);
     $resultingObj = $dataService->Add($theResourceObj);
     $error = $dataService->getLastError();
@@ -66,7 +69,7 @@ function accounting()
         $xmlBody = XmlObjectSerializer::getPostXmlFromArbitraryEntity($resultingObj, $urlResource);
         print $xmlBody . "\n";
     }
-    //Make jornal using the two accounts created
+    // Make jornal using the two accounts created
     $theResourceObj = JournalEntry::create([
         "Line" => [
             [
