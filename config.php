07891d646716322b35bc76f6702e91f3e6b7cdc2
abisalehalliprasan <anil_kumar3@intuit.com>
2018-05-02 17:35:23 -0700

    Adding Changes to Demo as per comments

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 return array(
     'authorizationRequestUrl' => 'https://appcenter.intuit.com/connect/oauth2',
     'tokenEndPointUrl' => 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
-    'client_id' => 'L0sbXOA0mmXSIaimPt9DaVx0MEjIvTYm11SwuRH2xVAERFTCir',
-    'client_secret' => 'jjsgrj9i6VljBEUOa02p6tHns1HW5wCY8e4xuNgk',
+    'client_id' => 'place your clientID',
+    'client_secret' => 'place your clientSecret',
     'oauth_scope' => 'com.intuit.quickbooks.accounting openid profile email phone address',
     'oauth_redirect_uri' => 'http://localhost:3000/callback.php',
 )
